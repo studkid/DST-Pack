8ec50d121f7434f05a68371331af07d1e09b5fee
Elly <eric.poore@gmail.com>
2023-08-22 19:51:39 -0500

    Pain

--- kubejs/startup_scripts/item_registry.js
+++ kubejs/startup_scripts/item_registry.js
@@ -1,6 +1,6 @@
 StartupEvents.registry('item', e => {
     e.create('hand_torch')
-        .texture('kubejs:item/torch.png')
+        .texture('kubejs:item/torch')
         .unstackable()
         .displayName("Hand Torch")
         .maxDamage(100);
