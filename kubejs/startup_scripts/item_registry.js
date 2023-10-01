a955a001dc8bfb82f361c6379b63be819a290e6c
Elly <eric.poore@gmail.com>
2023-10-01 02:45:11 -0500

    Update to 1.20.1

--- kubejs/startup_scripts/item_registry.js
+++
@@ -1,7 +0,0 @@
-StartupEvents.registry('item', e => {
-    e.create('hand_torch')
-        .texture('kubejs:item/torch')
-        .unstackable()
-        .displayName("Hand Torch")
-        .maxDamage(100);
-})
