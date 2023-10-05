60bec147f88f997d2da488727964285fb3f60dee
Elly <eric.poore@gmail.com>
2023-10-04 23:46:18 -0500

    Remove DRG Flare recipe

--- kubejs/server_scripts/disableLight.js
+++ kubejs/server_scripts/disableLight.js
@@ -3,10 +3,6 @@ ServerEvents.recipes(e => {
     e.remove({ input: 'minecraft:torch' });
     e.remove({ output: 'minecraft:soul_torch' });
     e.remove({ input: 'minecraft:soul_torch' });
-})
 
-ServerEvents.tags('item', event => {
-    const hide = ['minecraft:torch', 'minecraft:soul_torch', 'minecraft_lantern', 'minecraft:soul_lantern'];
-
-    
+    e.remove({ mod:"drg_flares" });
 })
