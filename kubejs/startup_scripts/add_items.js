a3b220f9a55f82f6719ff3d496db846cb6104fb2
Elly <eric.poore@gmail.com>
2023-10-05 13:58:27 -0500

    Added Lantern

--- kubejs/startup_scripts/add_items.js
+++ kubejs/startup_scripts/add_items.js
@@ -2,5 +2,10 @@ StartupEvents.registry('item', e => {
     e.create('hand_torch')
         .displayName('Torch')
         .texture('kubejs:item/hand_torch')
-        .tooltip('§4Cannot be Placed')
+        .tooltip('§4Cannot be Placed');
+    
+    e.create('hand_lantern')
+        .displayName('Lantern')
+        .texture('kubejs:item/hand_lantern')
+        .tooltip('§4Cannot be Placed');
 })
