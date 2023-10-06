13eabe24f9419652d47162f5a9748345c93ecc20
Treeway7 <56928485+Treeway7@users.noreply.github.com>
2023-10-05 20:39:54 -0500

    Sw. Snk. Golden Leggings recipe

---
+++ kubejs/server_scripts/swiftSneakGoldLeggings.js
@@ -0,0 +1,12 @@
+ServerEvents.recipes((event) => {
+    event.shaped(
+        Item.of('minecraft:golden_leggings', '{Damage:0}').enchant('minecraft:swift_sneak', 3),
+        [
+            'G G',
+            'G G',
+        ],
+        {
+            G: 'minecraft:gold_ingot',
+        },
+    ).id('minecraft:golden_leggings');
+});
