2e401f5cc74449570d589e594cf85d4437fbbb11
Treeway7 <56928485+Treeway7@users.noreply.github.com>
2023-10-06 12:06:50 -0500

    Added max stack size for lights

--- kubejs/startup_scripts/add_items.js
+++ kubejs/startup_scripts/add_items.js
@@ -2,10 +2,12 @@ StartupEvents.registry('item', (event) => {
     event.create('hand_torch')
         .displayName('Torch')
         .texture('kubejs:item/hand_torch')
-        .tooltip('§4Cannot be Placed');
+        .tooltip('§4Cannot be Placed')
+        .maxStackSize(1);
 
     event.create('hand_lantern')
         .displayName('Lantern')
         .texture('kubejs:item/hand_lantern')
-        .tooltip('§4Cannot be Placed');
+        .tooltip('§4Cannot be Placed')
+        .maxStackSize(1);
 });
