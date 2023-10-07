da68f0992d34e57f5d7cfb9524b12a81c7e94c94
Treeway7 <56928485+Treeway7@users.noreply.github.com>
2023-10-07 15:53:17 -0500

    Fixed pants

--- kubejs/server_scripts/swiftSneakGoldLeggings.js
+++ kubejs/server_scripts/swiftSneakGoldLeggings.js
@@ -2,6 +2,7 @@ ServerEvents.recipes((event) => {
     event.shaped(
         Item.of('minecraft:golden_leggings', '{Damage:0}').enchant('minecraft:swift_sneak', 3),
         [
+            'GGG',
             'G G',
             'G G',
         ],
